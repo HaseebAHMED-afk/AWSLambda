bf2bf6775660e539ab9b7a01a8441145c8f60adb
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-03-10 21:48:50 +0500

    Ready for deploy

--- lib/hello-lambda-stack.ts
+++ lib/hello-lambda-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
 
 export class HelloLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -6,6 +8,15 @@ export class HelloLambdaStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    const hello = new lambda.Function(this , 'HelloLambda' , {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler:'hello.handler'
+    })
+
+    new apigw.LambdaRestApi(this ,'Endpoint' , {
+      handler: hello
+    })
    
   }
 }
